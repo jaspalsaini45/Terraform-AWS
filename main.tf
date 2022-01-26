f87f8987e1efb3ea1f1dfcdd72a4aa11050727fc
jaspal <jaspalsaini45@gmail.com>
2022-01-26 11:52:10 +0300

    Terraform AWS EC2 Instance

---
+++ main.tf
@@ -0,0 +1,25 @@
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 3.0"
+    }
+  }
+}
+
+# Configure the AWS Provider
+provider "aws" {
+  region = "us-east-1"
+  access_key = "Key "
+  secret_key = "Secret"
+}
+
+# Create a Instance
+resource "aws_instance" "WebServer" {
+  ami           = "ami-04505e74c0741db8d"
+  instance_type = "t2.micro"
+
+  tags = {
+    Name = "HelloWorld"
+  }
+}
